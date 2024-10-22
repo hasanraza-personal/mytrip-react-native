69e56e8a5a202415035e7eba0c04a2709d3535e9
Hasan Raza <khan.hasanraza96@gmail.com>
2024-10-22 14:47:43 +0530

    Access to camera completed

---
+++ screens/AddPlace.js
@@ -0,0 +1,11 @@
+import PlaceForm from "../components/Places/PlaceForm";
+
+function AddPlace() {
+  return (
+    <>
+      <PlaceForm />
+    </>
+  );
+}
+
+export default AddPlace;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,11 @@
+import PlacesList from "../components/Places/PlacesList";
+
+function AllPlaces() {
+  return (
+    <>
+      <PlacesList />
+    </>
+  );
+}
+
+export default AllPlaces;
